cc92329011c829637471aa6b1a870fc9943ac67e
Steopa2001 <moraru495@gmail.com>
2025-07-30 17:55:54 +0200

    feat: accept user input from CLI

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+// Dichiaro una variabile chiamata 'sentence' 
+let sentence;
+
+// Controllo se è stato passato un argomento dal terminale (process.argv[2])
+if (process.argv[2]) {
+  // Se c'è un argomento, lo assegno alla variabile 'sentence'
+  sentence = process.argv[2];
+} else {
+  // Altrimenti, uso la frase di default
+  sentence = 'Hello boolean';
+}
+
+// Stampo la frase finale nel terminale
+console.log(sentence);
