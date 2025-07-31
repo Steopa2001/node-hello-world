f803fb5ee5a1b3cff90e6202a1733ba73a280b54
Steopa2001 <moraru495@gmail.com>
2025-07-31 09:44:05 +0200

    correction

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-// Dichiaro una variabile chiamata 'sentence' 
-let sentence;
 
-// Controllo se è stato passato un argomento dal terminale (process.argv[2])
-if (process.argv[2]) {
-  // Se c'è un argomento, lo assegno alla variabile 'sentence'
-  sentence = process.argv[2];
-} else {
-  // Altrimenti, uso la frase di default
-  sentence = 'Hello boolean';
-}
+let param = process[2] != undefined ? process.argv[2] : "Love Coding"
 
-// Stampo la frase finale nel terminale
-console.log(sentence);
+// // Dichiaro una variabile chiamata 'sentence' 
+// let sentence;
+
+// // Controllo se è stato passato un argomento dal terminale (process.argv[2])
+// if (process.argv[2] != undefined) {
+//   // Se c'è un argomento, lo assegno alla variabile 'sentence'
+//   sentence = process.argv[2];
+// } else {
+//   // Altrimenti, uso la frase di default
+//   sentence = 'Hello boolean';
+// }
+
+// // Stampo la frase finale nel terminale
+// console.log(sentence);
